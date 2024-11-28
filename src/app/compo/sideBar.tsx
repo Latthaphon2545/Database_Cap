1c61fe28c0999b3e520a9049485699b3d97bdd42
Latthaphon Phoemmanirat <108315515+Latthaphon2545@users.noreply.github.com>
2024-11-28 16:52:54 +0700

    Update sideBar.tsx

--- src/app/compo/sideBar.tsx
+++ src/app/compo/sideBar.tsx
@@ -1,4 +1,7 @@
+'use client'
+
 import Link from 'next/link'
+import { usePathname } from 'next/navigation' // Hook to get the current path
 import React from 'react'
 import { CiBoxList } from 'react-icons/ci'
 
@@ -11,12 +14,18 @@ const menu = [
 ]
 
 export default function SideBar() {
+  const pathname = usePathname()
   return (
     <ul className='menu bg-base-200  w-56'>
       {menu.map((item) => {
-        const active = item.link === window.location.pathname ? true : false
+        const active = item.link === pathname
         return (
-          <li key={item.name} className={`menu-item ${active ? 'active' : ''}`}>
+          <li
+            key={item.name}
+            className={`menu-item ${
+              active ? 'active text-primary outline-primary' : ''
+            }`}
+          >
             <Link href={item.link}>
               <div className='text-xl'>{item.icon}</div>
               {item.name}
